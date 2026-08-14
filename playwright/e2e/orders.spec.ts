f8f06d158dbf32414574e4dc599c7d70bf187d71
Jonathan Wollinger <jonathanwollinger@gmail.com>
2026-08-14 16:57:10 -0300

    feat: introduce feature actions architecture

---
+++ playwright/e2e/orders.spec.ts
@@ -0,0 +1,81 @@
+import { test } from '../support/fixtures'
+import { generateOrderCode } from '../support/helpers'
+import { OrderDetails } from '../support/actions/orderLookupActions'
+
+test.describe('Consulta de Pedido', () => {
+
+    test.beforeEach(async ({ app }) => {
+        await app.orderLookup.open()
+    })
+
+    test('deve consultar um pedido APROVADO', async ({ app }) => {
+        const order: OrderDetails = {
+            number: 'VLO-86R45Q',
+            status: 'APROVADO' as const,
+            color: 'Midnight Black',
+            wheels: 'sport Wheels',
+            customer: {
+                name: 'Jonathan Wollinger',
+                email: 'jonathanwollinger@gmail.com'
+            },
+            payment: 'À Vista'
+        }
+
+        await app.orderLookup.searchOrder(order.number)
+
+        await app.orderLookup.validateOrderDetails(order)
+        await app.orderLookup.validateStatusBadge(order.status)
+    })
+
+    test('deve consultar um pedido REPROVADO', async ({ app }) => {
+        const order: OrderDetails = {
+            number: 'VLO-QBJBF3',
+            status: 'REPROVADO' as const,
+            color: 'Glacier Blue',
+            wheels: 'sport Wheels',
+            customer: {
+                name: 'Teste Reprovado',
+                email: 'teste-reprovado@email.com'
+            },
+            payment: 'À Vista'
+        }
+
+        await app.orderLookup.searchOrder(order.number)
+
+        await app.orderLookup.validateOrderDetails(order)
+        await app.orderLookup.validateStatusBadge(order.status)
+    })
+
+    test('deve consultar um pedido EM ANALISE', async ({ app }) => {
+        const order: OrderDetails = {
+            number: 'VLO-83Q38G',
+            status: 'EM_ANALISE' as const,
+            color: 'Lunar White',
+            wheels: 'sport Wheels',
+            customer: {
+                name: 'Teste Em Analise',
+                email: 'teste-em-analise@email.com'
+            },
+            payment: 'À Vista'
+        }
+
+        await app.orderLookup.searchOrder(order.number)
+
+        await app.orderLookup.validateOrderDetails(order)
+        await app.orderLookup.validateStatusBadge(order.status)
+    })
+
+    test('deve exibir mensagem de erro ao consultar um pedido inexistente', async ({ app }) => {
+        const orderNumber = generateOrderCode()
+
+        await app.orderLookup.searchOrder(orderNumber)
+        await app.orderLookup.validateOrderNotFound()
+    })
+
+    test('deve exibir mensagem de erro ao consultar um pedido fora do padrao', async ({ app }) => {
+        const orderNumber = 'COD INVÁLIDO'
+
+        await app.orderLookup.searchOrder(orderNumber)
+        await app.orderLookup.validateOrderNotFound()
+    })
+})
---
+++ playwright/support/actions/landingActions.ts
@@ -0,0 +1,11 @@
+import { expect, Page } from '@playwright/test'
+
+export function createLandingActions(page: Page) {
+    return {
+        async goto() {
+            await page.goto('/')
+            const title = page.getByTestId('hero-section').getByRole('heading')
+            await expect(title).toContainText('Velô Sprint')
+        },
+    }
+}
