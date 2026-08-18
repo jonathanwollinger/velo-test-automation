43184544c728d3ff6c8362eb4f5af7ca0f639b2d
Jonathan Wollinger <jonathanwollinger@gmail.com>
2026-08-17 23:07:28 -0300

    refactor: improve configurator actions

--- playwright/support/actions/configuratorActions.ts
+++ playwright/support/actions/configuratorActions.ts
@@ -3,6 +3,12 @@ import { expect, Page } from '@playwright/test'
 type CarOptional = 'precision-park' | 'flux-capacitor'
 
 export function createConfiguratorActions(page: Page) {
+    const optionalCheckbox = (optional: CarOptional) =>
+        page.getByTestId(`opt-${optional}`)
+
+    const wheelOption = (wheel: string) =>
+        page.getByTestId(`wheel-option-${wheel}`)
+
     return {
         async open() {
             await page.goto('/configure')
@@ -12,40 +18,40 @@ export function createConfiguratorActions(page: Page) {
             await page.getByRole('button', { name }).click()
         },
 
-        async selectWhells(name: string | RegExp) {
-            await page.getByRole('button', { name }).click()
+        async selectWheels(wheelName: string) {
+            await expect(wheelOption(wheelName)).toBeVisible()
+            await wheelOption(wheelName).click()
         },
 
-        async setOptional(optional: CarOptional, enabled: boolean) {
-            const optionalCheckbox = page.getByTestId(`opt-${optional}`)
-        
-            await expect(optionalCheckbox).toBeVisible()
-        
-            if (await optionalCheckbox.isChecked() !== enabled) {
-                await optionalCheckbox.click()
-            }
-        
-            await expect(optionalCheckbox).toBeChecked({
-                checked: enabled,
-            })
+        async checkOptional(optional: CarOptional) {
+            await expect(optionalCheckbox(optional)).toBeVisible()
+            await optionalCheckbox(optional).check()
+        },
+
+        async uncheckOptional(optional: CarOptional) {
+            await expect(optionalCheckbox(optional)).toBeVisible()
+            await optionalCheckbox(optional).uncheck()
         },
 
         async expectTotalPrice(price: string) {
             const priceElement = page.getByTestId('total-price')
+
             await expect(priceElement).toBeVisible()
             await expect(priceElement).toHaveText(price)
         },
 
         async expectCarImageSrc(src: string) {
             const carImage = page.getByTestId('car-exterior-image')
+
             await expect(carImage).toHaveAttribute('src', src)
         },
 
-        async goToCheckout() {
+        async finishConfigurator() {
             const checkoutButton = page.getByTestId('checkout-button')
+
             await expect(checkoutButton).toBeVisible()
             await checkoutButton.click()
-            await expect(page).toHaveURL(/\/order$/)
+            await expect(page).toHaveURL('/order')
         },
     }
 }
